4005ef2104b40c4003a77a4a00d91786ea257ec8
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2023-02-10 15:53:24 +0200

    home task 1 Angular

---
+++ src/app/billgates/billgates.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BillgatesComponent } from './billgates.component';
+
+describe('BillgatesComponent', () => {
+  let component: BillgatesComponent;
+  let fixture: ComponentFixture<BillgatesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BillgatesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BillgatesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/billgates/billgates.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billgates',
+  templateUrl: './billgates.component.html',
+  styleUrls: ['./billgates.component.css']
+})
+export class BillgatesComponent {
+
+}
